5f3cdf1271b986e33f3aaa0ade61d8bbcb21e942
Gibson Joseph <gibsonjoseph890@gmail.com>
2024-01-16 14:49:27 +0530

    feat: Add success and failure upload popups

---
+++ src/components/components/FileUploadError/FileUploadError.tsx
@@ -0,0 +1,36 @@
+import { memo } from "react";
+import { BiError } from "react-icons/bi";
+const FileUploadError = ({ handleClick }: { handleClick: () => void }) => {
+  return (
+    <div className="max-w-lg w-full p-5 bg-white flex flex-col justify-center items-center shadow-lg rounded-lg">
+      <div className="flex flex-col justify-center items-center">
+        <div className="rounded-full border-white text-red-600 w-24 h-24">
+          <BiError className=" w-full h-full" />
+        </div>
+        <h3 className="text-2xl font-[PublicSans] text-red-600 font-semibold mb-5">
+          ERROR!
+        </h3>
+        <div className="text-center space-y-1">
+          <p className="text-base font-medium font-[PublicSans]">
+            We are unable to continue the process.
+          </p>
+          <p className="text-[#818181] font-medium font-[PublicSans]">
+            Please try again to complete the process.
+          </p>
+        </div>
+      </div>
+      <div className=" w-full flex justify-center items-center mt-2">
+        <button
+          type="button"
+          title="open pdf"
+          className="bg-red-600 hover:bg-red-700 py-2 px-14 mt-2 font-[PublicSans] font-medium text-[#fff] transition-all duration-300"
+          onClick={() => handleClick()}
+        >
+          Try Again
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default memo(FileUploadError);
---
+++ src/components/components/FileUploadSuccess/FileUploadSuccess.tsx
@@ -0,0 +1,51 @@
+import { MdCancel } from "react-icons/md";
+import { FiCheckCircle } from "react-icons/fi";
+import { memo } from "react";
+
+const FileUploadSuccess = ({
+  handleClick,
+  handleCancelPopUp,
+}: {
+  handleClick: () => void;
+  handleCancelPopUp: () => void;
+}) => {
+  return (
+    <div className="max-w-lg w-full p-5 bg-white flex flex-col justify-center items-center shadow-lg rounded-lg">
+      <div className="w-full flex justify-end">
+        <MdCancel
+          className="w-7 h-7 text-[#ddd] cursor-pointer hover:text-[#acacac] transition-all duration-300"
+          title="cancel"
+          onClick={() => handleCancelPopUp()}
+        />
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <div className="rounded-full border-white text-green-600 w-24 h-24">
+          <FiCheckCircle className=" w-full h-full" />
+        </div>
+        <h3 className="text-2xl font-[PublicSans] text-green-600 font-semibold mb-5">
+          SUCCESS!
+        </h3>
+        <div className="text-center space-y-1">
+          <p className="text-base font-medium font-[PublicSans]">
+            Your file have been uploaded successfully
+          </p>
+          <p className="text-[#818181] font-medium font-[PublicSans]">
+            Your file is now available
+          </p>
+        </div>
+      </div>
+      <div className=" w-full flex justify-center items-center mt-2">
+        <button
+          type="button"
+          title="open pdf"
+          className="bg-green-600 hover:bg-green-700 py-2 px-14 mt-2 font-[PublicSans] font-medium text-[#fff] transition-all duration-300"
+          onClick={() => handleClick()}
+        >
+          Click to open
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default memo(FileUploadSuccess);
