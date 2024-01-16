28bac96c4fb2af52de96e6d17905fc7eb58b18a7
Gibson Joseph <gibsonjoseph890@gmail.com>
2024-01-16 18:24:34 +0530

    style: Update styling for the header component

--- src/components/components/HeaderComponent/HeaderComponent.tsx
+++ src/components/components/HeaderComponent/HeaderComponent.tsx
@@ -20,19 +20,19 @@ const HeaderComponent = () => {
   };
 
   return (
-    <header className="bg-gray-600 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-xl font-bold font-[PublicSans]">
+    <header className="p-4 border-b">
+      <div className="container mx-auto flex justify-between items-center transition-all duration-300">
+        <div className="text-slate-600 text-xl font-medium font-[PublicSans]">
           Our Logo
         </div>
         <div className="flex items-center gap-x-4">
           <button
             type="button"
-            className="text-white flex items-center font-[PublicSans]"
-            onClick={handleLogout}
+            className="text-white flex items-center font-[PublicSans] bg-slate-600 py-1 px-2 border-2 border-slate-600 rounded-md hover:bg-white hover:text-slate-600 transition-all duration-300"
+            onClick={() => handleLogout}
           >
-            <FaSignOutAlt className="mr-2" />
-            Logout
+            <FaSignOutAlt className="mr-2 w-5 h-5" />
+            <span>Logout</span>
           </button>
         </div>
       </div>
